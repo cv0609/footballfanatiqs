f7ba5932680597dd3090142b11edb206afc6ffe7
karan <kn@int.iqsay.com>
2025-01-17 16:56:15 +0530

    fixed

--- resources/views/front_end/pages/football/team-view.blade.php
+++ resources/views/front_end/pages/football/team-view.blade.php
@@ -194,6 +194,35 @@
            
         </div>
         <!-- Modal Success -->
+
+
+        <!-- alert- login msg popup start -->
+<div class="alert-msg">
+    <div class="modal fade" id="loginModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
+                            fill="#d30f0f">
+                            <path
+                                d="M480-280q17 0 28.5-11.5T520-320q0-17-11.5-28.5T480-360q-17 0-28.5 11.5T440-320q0 17 11.5 28.5T480-280Zm-40-160h80v-240h-80v240Zm40 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
+                            </svg>
+                    </span>
+                    <h4 id="login-message"></h4>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" id="login-click" class="btn btn-secondary"
+                        data-bs-dismiss="modal">Login</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- alert-msg popup end -->
 @endsection
 
 @section('custom-script')
